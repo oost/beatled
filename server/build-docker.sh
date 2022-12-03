4e0eddd45df892d01107773be912302d04015132
Oscar Ostlund <oscar.ostlund@gmail.com>
2022-12-03 18:09:40 -0500

    Use ssh

--- server/build-docker.sh
+++ server/build-docker.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
 rm -rf ../build
-
 cmake -G Ninja -DCMAKE_TOOLCHAIN_FILE=/home/source/docker-toolchain.cmake -DVCPKG_DIR=${VCPKG_HOME}  -DVCPKG_TARGET_TRIPLET=${TARGET_TRIPLET} -B ../build -S .
-
 cmake --build ../build/
